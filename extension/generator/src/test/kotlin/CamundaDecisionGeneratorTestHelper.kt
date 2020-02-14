5a78eb599a59746e34ce572b3e36b401bc2b53d5
Jan Galinski <jan.galinski@holisticon.de>
2020-02-14 16:56:40 +0100

    introduce lib/test

---
+++ extension/generator/src/test/kotlin/CamundaDecisionGeneratorTestHelper.kt
@@ -0,0 +1,17 @@
+package io.holunda.decision.generator
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+// empty
+
+
+
+class DummyTest {
+
+
+  @Test
+  fun name() {
+    Assertions.assertThat(1 < 2).isTrue()
+  }
+}
---
+++ lib/test/src/main/kotlin/CamundaDecisionTestLib.kt
@@ -0,0 +1,3 @@
+package io.holunda.decision.lib.test
+
+// marker
