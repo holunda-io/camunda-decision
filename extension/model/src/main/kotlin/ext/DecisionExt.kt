f1a693a4a23948f6358004e6269dfe24b441979d
Jan Galinski <jan.galinski@holisticon.de>
2020-03-03 00:45:02 +0100

    support aggregation on collect

--- extension/model/src/main/kotlin/ext/DecisionExt.kt
+++ extension/model/src/main/kotlin/ext/DecisionExt.kt
@@ -1,11 +1,15 @@
 package io.holunda.decision.model.ext
 
+import org.camunda.bpm.model.dmn.BuiltinAggregator
 import org.camunda.bpm.model.dmn.HitPolicy
 import org.camunda.bpm.model.dmn.instance.Decision
 import org.camunda.bpm.model.dmn.instance.DecisionTable
 
 fun Decision.getDecision() : DecisionTable = this.expression as DecisionTable
 
-fun Decision.decisionTable(hitPolicy: HitPolicy) = this.addChildElement(DecisionTable::class).apply {
+fun Decision.decisionTable(hitPolicy: HitPolicy, aggregation:BuiltinAggregator? = null ) = this.addChildElement(DecisionTable::class).apply {
   this.hitPolicy = hitPolicy
+  if (hitPolicy == HitPolicy.COLLECT) {
+    this.aggregation = aggregation
+  }
 }
