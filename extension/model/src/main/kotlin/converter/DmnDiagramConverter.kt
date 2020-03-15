21c7d46b9cdb47967b666877b7390497d8e4da62
Jan Galinski <jan.galinski@holisticon.de>
2020-03-15 01:09:51 +0100

    add docs

--- extension/model/src/main/kotlin/converter/DmnDiagramConverter.kt
+++ extension/model/src/main/kotlin/converter/DmnDiagramConverter.kt
@@ -1,10 +1,12 @@
 package io.holunda.decision.model.converter
 
 import io.holunda.decision.model.element.DmnDiagram
-import io.holunda.decision.model.ext.*
+import io.holunda.decision.model.ext.addChildElement
+import io.holunda.decision.model.ext.definitions
+import io.holunda.decision.model.ext.extensionElement
+import io.holunda.decision.model.ext.findDecisionByKey
 import org.camunda.bpm.model.dmn.Dmn
 import org.camunda.bpm.model.dmn.DmnModelInstance
-import org.camunda.bpm.model.dmn.instance.ExtensionElements
 import org.camunda.bpm.model.dmn.instance.InformationRequirement
 
 /**
@@ -12,23 +14,31 @@ import org.camunda.bpm.model.dmn.instance.InformationRequirement
  */
 object DmnDiagramConverter  {
 
+  /**
+   * Creates a camunda DmnModelInstance from a given DmnDiagram.
+   */
   fun toModelInstance(dmnDiagram: DmnDiagram): DmnModelInstance = Dmn.createEmptyModel().apply {
     val definitions = definitions(dmnDiagram.name, dmnDiagram.id)
+
+    // first, create all decision (tables) to the model instance.
     dmnDiagram.decisionTables.forEach {
       DmnDecisionTableConverter.toModelInstance(definitions, it)
     }
 
+    // then: calculate the layout ...
     val layout = DmnDiagramLayout(dmnDiagram.decisionDefinitionKeys, dmnDiagram.requiredDecisions).layout().values
 
+    // ... add coordinates to all decisions, if is is part of a graph: render arrow
     for (box in layout) {
       definitions.findDecisionByKey(box.key)?.extensionElement(box)
     }
 
+    // ... and if the decision is part of a graph: add InformationRequirement.
     dmnDiagram.requiredDecisions.forEach { target ->
-      val t = requireNotNull(definitions.findDecisionByKey(target.key))
+      val decision = requireNotNull(definitions.findDecisionByKey(target.key))
 
       target.value.forEach {sourceKey ->
-        t.addChildElement(InformationRequirement::class).apply {
+        decision.addChildElement(InformationRequirement::class).apply {
           requiredDecision = requireNotNull(definitions.findDecisionByKey(sourceKey))
         }
       }
