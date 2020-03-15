6f2daf5aaa01c44061046a94f444aa98431383ca
Jan Galinski <jan.galinski@holisticon.de>
2020-03-15 01:01:51 +0100

    adding docs

--- extension/model/src/main/kotlin/ext/RuleExt.kt
+++ extension/model/src/main/kotlin/ext/RuleExt.kt
@@ -5,15 +5,23 @@ import org.camunda.bpm.model.dmn.instance.InputEntry
 import org.camunda.bpm.model.dmn.instance.OutputEntry
 import org.camunda.bpm.model.dmn.instance.Rule
 
-
+/**
+ * Creates an InputEntry on Rule and sets the given text value.
+ */
 fun Rule.inputEntry(value: String?) = this.addChildElement(InputEntry::class).apply {
   text = textContent(value)
 }
 
+/**
+ * Creates an OutputEntry on Rule and sets the given text value.
+ */
 fun Rule.outputEntry(value: String?) = this.addChildElement(OutputEntry::class).apply {
   text = textContent(value)
 }
 
+/**
+ * Adds a description (aka annotation) to the Rule.
+ */
 fun Rule.description(value:String?) = this.addChildElement(Description::class).apply {
   textContent = value
 }
