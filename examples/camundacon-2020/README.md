74b30317d364be7a2aab5bb4965f51b7563a2cfe
Jan Galinski <jan.galinski@holisticon.de>
2020-10-08 11:36:24 +0200

    prepare rest calls

--- examples/camundacon-2020/README.md
+++ examples/camundacon-2020/README.md
@@ -1,5 +1,12 @@
 # Demo
 
+## links
+
+* cockpit: <http://localhost:10110/> (admin/admin)
+* swagger: <http://localhost:10110/swagger-ui.html>
+
+## Plan
+
 * show project layout
   * don't be afraid: it's kotlin
   * ordinary spring boot app using camunda starter
@@ -10,4 +17,12 @@
 
 * show cockpit: no auto deployment, app is fresh and empty
 
+* deploy the single `legal_age.dmn`
 
+## Data
+
+```kotlin
+CustomerEntity(id = "1", name = "Peter", age = 19, sex = "male", country = "USA", state = "Alabama")
+CustomerEntity(id = "2", name = "Paul", age = 17, sex = "male", country = "Pakistan")
+CustomerEntity(id = "3", name = "Mary", age = 17, sex = "female", country = "Pakistan")
+```
--- examples/camundacon-2020/src/main/kotlin/rest/CustomerController.kt
+++ examples/camundacon-2020/src/main/kotlin/rest/CustomerController.kt
@@ -4,6 +4,7 @@ import io.holunda.decision.example.camundacon2020.domain.CustomerEntity
 import io.holunda.decision.example.camundacon2020.domain.CustomerRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,5 +18,5 @@ class CustomerController(
   fun findAll() : ResponseEntity<List<CustomerEntity>> = ResponseEntity.ok(repository.findAll())
 
   @GetMapping(path = ["/{customerId}"])
-  fun findById(customerId:String) : ResponseEntity<CustomerEntity> = ResponseEntity.of(repository.findById(customerId))
+  fun findById(@PathVariable("customerId") customerId:String) : ResponseEntity<CustomerEntity> = ResponseEntity.of(repository.findById(customerId))
 }
